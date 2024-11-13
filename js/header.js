a429ce51bf4eb2814a2df343667b536fef32bb6b
P-aan0901 <22130004@st.hcmuaf.edu.vn>
2024-11-13 22:50:28 +0700

    Create header.js

---
+++ js/header.js
@@ -0,0 +1,13 @@
+document.getElementById("account-menu").addEventListener("click", function(event) {
+    event.stopPropagation(); // Ngăn chặn việc sự kiện click lan ra ngoài
+    const dropdownMenu = this.querySelector(".dropdown-menu");
+    dropdownMenu.style.display = dropdownMenu.style.display === "block" ? "none" : "block";
+});
+
+// Đóng dropdown khi nhấp ra ngoài
+document.addEventListener("click", function(event) {
+    const dropdownMenu = document.querySelector("#account-menu .dropdown-menu");
+    if (dropdownMenu) {
+        dropdownMenu.style.display = "none";
+    }
+});
