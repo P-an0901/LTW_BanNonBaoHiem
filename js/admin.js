b3dd0b1c7650c5fe81ff0418bef4f8e5c0cfa253
P-aan0901 <22130004@st.hcmuaf.edu.vn>
2024-11-15 13:32:06 +0700

    Create admin.js

---
+++ js/admin.js
@@ -0,0 +1,19 @@
+const sidebars = document.querySelectorAll(".sidebar-list-item.tab-content");
+const sections = document.querySelectorAll(".section");
+
+for(let i = 0; i < sidebars.length; i++) {
+    sidebars[i].onclick = function () {
+        document.querySelector(".sidebar-list-item.active").classList.remove("active");
+        document.querySelector(".section.active").classList.remove("active");
+        sidebars[i].classList.add("active");
+        sections[i].classList.add("active");
+    };
+}
+
+const closeBtn = document.querySelectorAll('.section');
+console.log(closeBtn[0])
+for(let i=0;i<closeBtn.length;i++){
+    closeBtn[i].addEventListener('click',(e) => {
+        sidebar.classList.add("open");
+    })
+}
