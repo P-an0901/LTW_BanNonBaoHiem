84c1139cc1de0ca8dee47f4553988daddc0043ba
P-aan0901 <22130004@st.hcmuaf.edu.vn>
2024-11-22 16:12:48 +0700

    cập nhật tất cả

--- js/giohang.js
+++ js/giohang.js
@@ -27,7 +27,6 @@ function updateCartDisplay() {
     // Cập nhật danh sách sản phẩm trong giỏ hàng
     if (cartItemsContainer) {
         cartItemsContainer.innerHTML = '';
-
         if (carts.length > 0) {
             emptyMessage.style.display = 'none';
             carts.forEach((product, index) => {
@@ -43,8 +42,6 @@ function updateCartDisplay() {
                 `;
                 cartItemsContainer.innerHTML += productHTML;
             });
-        } else {
-            emptyMessage.style.display = 'block';
         }
     }
 
@@ -69,5 +66,5 @@ function saveCart() {
 }
 function redirectToCheckout() {
     saveCart(); // Lưu giỏ hàng vào localStorage
-    window.location.href = "../html/checkout.html"; // Chuyển hướng sang trang thanh toán
+    window.location.href = "checkout.html"; // Chuyển hướng sang trang thanh toán
 }
