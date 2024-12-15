c98e1d57a957b220c06c18e9c725ba7ca44fe1a7
P-aan0901 <22130004@st.hcmuaf.edu.vn>
2024-12-15 19:15:05 +0700

    Create BrandDao.java

---
+++ src/main/java/helmet/vn/ltw_bannonbaohiem/dao/BrandDao.java
@@ -0,0 +1,71 @@
+package helmet.vn.ltw_bannonbaohiem.dao;
+
+import helmet.vn.ltw_bannonbaohiem.dao.db.JdbiConnect;
+import helmet.vn.ltw_bannonbaohiem.dao.model.Brand;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.statement.Update;
+import org.jdbi.v3.core.result.ResultIterable;
+import java.util.List;
+
+public class BrandDao {
+    private Jdbi jdbi;
+
+    public BrandDao() {
+        this.jdbi = JdbiConnect.get();
+    }
+    public boolean addBrand(String name, String imageUrl) {
+        String sql = "INSERT INTO brands (name, imageUrl) VALUES (?, ?)";
+        try {
+            return jdbi.withHandle(handle -> {
+                Update update = handle.createUpdate(sql);
+                update.bind(0, name);
+                update.bind(1, imageUrl);
+                int rowsAffected = update.execute();
+                return rowsAffected > 0;
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public List<Brand> getAllBrands() {
+        String sql = "SELECT * FROM brands";
+        return jdbi.withHandle(handle -> {
+            return handle.createQuery(sql)
+                    .mapToBean(Brand.class)
+                    .list();
+        });
+    }
+    public Brand getBrandById(int id) {
+        String sql = "SELECT * FROM brands WHERE id = ?";
+        return jdbi.withHandle(handle -> {
+            return handle.createQuery(sql)
+                    .bind(0, id)
+                    .mapTo(Brand.class)
+                    .findOne()
+                    .orElse(null);
+        });
+    }
+    public void updateBrand(int id, String name, String imageUrl) {
+        String sql = "UPDATE brands SET name = ?, imageUrl = ? WHERE id = ?";
+        jdbi.useHandle(handle -> {
+            Update update = handle.createUpdate(sql);
+            update.bind(0, name);
+            update.bind(1, imageUrl);
+            update.bind(2, id);
+            update.execute();
+        });
+    }
+    public void deleteBrand(int id) {
+        String sql = "DELETE FROM brands WHERE id = ?";
+        jdbi.useHandle(handle -> {
+            Update update = handle.createUpdate(sql);
+            update.bind(0, id);
+            update.execute();
+        });
+    }
+
+}
+
