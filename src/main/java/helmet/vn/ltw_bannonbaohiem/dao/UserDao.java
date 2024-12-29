da587478fd86d4021f31f3242240ace8ca2a9254
P-aan0901 <22130004@st.hcmuaf.edu.vn>
2024-12-29 17:39:43 +0700

    chức năng register

--- src/main/java/helmet/vn/ltw_bannonbaohiem/dao/UserDao.java
+++ src/main/java/helmet/vn/ltw_bannonbaohiem/dao/UserDao.java
@@ -3,6 +3,7 @@ package helmet.vn.ltw_bannonbaohiem.dao;
 import helmet.vn.ltw_bannonbaohiem.dao.db.JdbiConnect;
 import helmet.vn.ltw_bannonbaohiem.dao.model.User;
 import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.statement.Update;
 
 public class UserDao {
     private Jdbi jdbi;
@@ -19,5 +20,28 @@ public class UserDao {
                     .findFirst().orElse(null);
         });
 
+    }
+
+    public boolean register(String username, String hashedPassword, String name, String email) {
+        if (email == null || email.trim().isEmpty()) {
+            email = null;
+        }
+        String sql = "INSERT INTO users(username, email, password, fullName) " +
+                "VALUES(?, ?, ?, ?)";
+        try {
+            String finalEmail = email;
+            return jdbi.withHandle(handle -> {
+                Update update = handle.createUpdate(sql);
+                update.bind(0, username);
+                update.bind(1, finalEmail);
+                update.bind(2, hashedPassword);
+                update.bind(3, name);
+                int rowsAffected = update.execute();
+                return rowsAffected > 0;
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
